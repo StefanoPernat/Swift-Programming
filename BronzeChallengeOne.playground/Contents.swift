824d44e49dbb065198177bd231cefb09c3f1452b
Stefano Pernat <stefano.pernat@gmail.com>
2017-03-16 20:46:58 +0100

    Finished Bronze Challenge One

---
+++ BronzeChallengeOne.playground/Contents.swift
@@ -0,0 +1,11 @@
+/*
+ BRONZE CHALLENGE ONE
+ 
+ Create a new playground, create a new instance of the String type.
+ Set the value of this instance to be equal to your lastname
+ Print its value to the console
+ */
+import Cocoa
+
+var mylastname = "Pernat"
+print(mylastname)
