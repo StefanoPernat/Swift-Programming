8d53bf8d09604ce3435fdc45173ea37b3a531dc0
Stefano Pernat <stefano.pernat@gmail.com>
2017-03-16 20:38:09 +0100

    Added First Playground and gitignore

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"           //  variable of type String, its inferred by the compiler
+str += "!"
+print(str)                              //  print function prints to the console
