6a729f11705764e889d6faf7e656744e28b492a7
Stefano Pernat <stefano.pernat@gmail.com>
2017-04-09 09:57:11 +0200

    Chapter 11, Bronze and Silver Challenge

--- Groceries.playground/Contents.swift
+++ Groceries.playground/Contents.swift
@@ -26,3 +26,13 @@ let itemsToReturn = commonGroceryBag.intersection(roommatesGroceryBag)
 let yourSecondBag = Set(["Barries", "Yogurt"])
 let roommateSecondBag = Set(["Grapes", "Honey"])
 let disjoint = yourSecondBag.isDisjoint(with: roommateSecondBag)
+
+//  Bronze Challenge
+let myCities = Set(["Atlanta", "Chicago", "Jacksonville", "New York", "San Francisco"])
+let yourCities = Set(["Chicago", "San Francisco", "Jacksonville"])
+
+yourCities.isSubset(of: myCities)
+
+//  Silver Challenge
+groceriesBag.formUnion(friendGroceryBag)
+groceriesBag
